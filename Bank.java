f76b4b4e8f13801b914644ef1a27439ad5a54437
rafiody16 <rafiody16@gmail.com>
2023-09-13 20:19:08 +0700

    Jobsheet3

---
+++ Bank.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bank {
+
+    public static void main (String [] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+
+        int jml_tabungan_awal, lama_menabung;
+        double jml_tabungan_akhir, bunga, prosentase_bunga = 0.02;
+
+            System.out.println("Masukkan jumlah tabungan awal: ");
+            jml_tabungan_awal = input.nextInt();
+            System.out.println("Masukkan lama menabung anda: ");
+            lama_menabung = input.nextInt();
+
+            bunga = lama_menabung * prosentase_bunga *jml_tabungan_awal;
+            jml_tabungan_akhir = bunga + jml_tabungan_awal;
+
+            System.out.println("Jumlah tabungan akhir: " + jml_tabungan_akhir);
+
+    }
+    
+}
---
+++ ContohVariabel22.java
@@ -0,0 +1,22 @@
+/**
+ * ContohVariabel22
+ */
+public class ContohVariabel22 {
+
+    public static void main(String [] args){
+
+        String hobi = "Bermain Bulutangkis";
+        boolean isPandai = true;
+        byte umur = 20;
+        char jenisKelamin = 'L';
+        double $ipk = 4.00, tinggi = 1.70;
+
+            System.out.println("Hobi: " + hobi);
+            System.out.println("Apakah anda pandai: " + isPandai);
+            System.out.println("Jenis kelamin: " + jenisKelamin);
+            System.out.println("Umur saya saat ini: " + umur);
+            System.out.println(String.format("Saya berIPK %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+    
+}
